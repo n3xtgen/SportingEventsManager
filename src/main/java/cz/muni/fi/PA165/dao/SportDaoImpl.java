7f0f3d56f2cf2850776c219ed71dffb6da4f0479
vlado <vladoe12@hotmail.com>
2015-10-25 10:45:55 +0100

    implement SportDaoImpl

--- src/main/java/cz/muni/fi/PA165/dao/SportDaoImpl.java
+++ src/main/java/cz/muni/fi/PA165/dao/SportDaoImpl.java
@@ -2,29 +2,41 @@ package cz.muni.fi.PA165.dao;
 
 import cz.muni.fi.PA165.entity.Sport;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 /**
  * Created by Vladimir on 25.10.2015.
  */
 public class SportDaoImpl implements SportDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
     public Sport findById(long id) {
-        return null;
+        return em.find(Sport.class, id);
     }
 
     public void create(Sport s) {
-
+        em.persist(s);
     }
 
     public void delete(Sport s) {
-
+        em.remove(s);
     }
 
     public List<Sport> findAll() {
-        return null;
+        return em.createQuery("SELECT s FROM Sport S", Sport.class).getResultList();
     }
 
     public Sport findByName(String name) {
-        return null;
+        try {
+            return em.createQuery("SELECT s FROM Sport S WHERE name = :name",Sport.class).setParameter(":name", name)
+                    .getSingleResult();
+        } catch (NoResultException nrf) {
+            return null;
+        }
     }
 }
