3dff11c45ac2ae361895bc410b8d47f8da2276d7
Vlado <elias@master.cz>
2015-10-27 11:56:55 +0100

    javadoc for SportDao

--- src/main/java/cz/muni/fi/PA165/dao/SportDao.java
+++ src/main/java/cz/muni/fi/PA165/dao/SportDao.java
@@ -9,9 +9,35 @@ import java.util.List;
  */
 
 public interface SportDao {
+    /**
+     * Find sport by ID
+     * @param id represent id of sport
+     * @return Sport with selected ID
+     */
     public Sport findById(long id);
+
+    /**
+     * Create new sport
+     * @param s represent specific sport
+     */
     public void create(Sport s);
+
+    /**
+     * Delete existing sport
+     * @param s represent specific sport
+     */
     public void delete(Sport s);
+
+    /**
+     * Find all sports
+     * @return all Sports from database
+     */
     public List<Sport> findAll();
+
+    /**
+     * Find sport by name
+     * @param name represent name of sport
+     * @return Sport with selected name
+     */
     public Sport findByName(String name);
 }
