12fe912af525ceec247d0b79477897599db61e5f
Jan Bouska <xbouska1@fi.muni.cz>
2015-10-28 00:17:21 +0100

    add exception handling in SportsmanDao

--- src/main/java/cz/muni/fi/PA165/dao/SportsmanDao.java
+++ src/main/java/cz/muni/fi/PA165/dao/SportsmanDao.java
@@ -14,42 +14,47 @@ public interface SportsmanDao {
 	/**
 	 *
 	 * @param id
-	 * @return sportsman
-	 * @throws NoResultException if there is no result
+	 * @return sportsman or null if the sportsman does not exist
+	 * @throws IllegalArgumentException if the first argument does not denote an entity type or
+	 * 			the second argument is is not a valid type for that entity’s primary key or is null
+	 *
 	 */
 	public Sportsman findById(Long id);
 
 	/**
 	 *
 	 * @param s sportsman to create
+	 * @throws cz.muni.fi.PA165.dao.exception.DataAccessException if there is a problem on persistent layer
 	 */
 	public void create(Sportsman s);
 
 	/**
 	 *
 	 * @param s sportsman to delete
+	 * @throws cz.muni.fi.PA165.dao.exception.DataAccessException if there is a problem on persistent layer
 	 */
 	public void delete(Sportsman s);
 
 	/**
 	 *
 	 * @return all artifact from database
+	 * @throws cz.muni.fi.PA165.dao.exception.DataAccessException if there is a problem on persistent layer
 	 */
 	public List findAll();
 
 	/**
 	 *
 	 * @param surname
-	 * @return sportsman with selected surname
-	 * @throws NoResultException if there is no result
+	 * @return sportsmans with selected surname
+	 * @throws cz.muni.fi.PA165.dao.exception.DataAccessException if there is a problem on persistent layer
 	 */
 	public List<Sportsman> findBySurname(String surname);
 
 	/**
 	 *
 	 * @param id
-	 * @return sportsman with selected ID
-	 * @throws NoResultException if there is no result
+	 * @return sportsman with selected ID or null if the sportsman does not exist
+	 * @throws cz.muni.fi.PA165.dao.exception.DataAccessException if there is a problem on persistent layer
 	 *
 	 */
 	public Sportsman findByPersonalID(String id);
--- src/main/java/cz/muni/fi/PA165/dao/SportsmanDaoImpl.java
+++ src/main/java/cz/muni/fi/PA165/dao/SportsmanDaoImpl.java
@@ -1,15 +1,17 @@
 
 package cz.muni.fi.PA165.dao;
 
-import cz.muni.fi.PA165.entity.Sportsman;
-import cz.muni.fi.PA165.entity.Event;
-
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
-import javax.persistence.TypedQuery;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import cz.muni.fi.PA165.dao.exception.DataAccessException;
+import cz.muni.fi.PA165.entity.Event;
+import cz.muni.fi.PA165.entity.Sportsman;
 
 /**
  *
@@ -28,29 +30,51 @@ public class SportsmanDaoImpl implements SportsmanDao{
 
     @Override
     public void create(Sportsman s) {
-        em.persist(s);
+        try {
+            em.persist(s);
+        } catch (Throwable tr) {
+            throw new DataAccessException("Problem with creating sportsman:" + s, tr);
+        }
     }
 
     @Override
     public void delete(Sportsman s) {
+        try {
         em.remove(s);
+        } catch (Throwable tr) {
+            throw new DataAccessException("Problem with deleting sportsman:" + s, tr);
+        }
     }
 
     @Override
     public List<Sportsman> findAll() {
-        TypedQuery<Sportsman> query = em.createQuery("SELECT s FROM Sportsman s", Sportsman.class);
-				return query.getResultList();
+        try {
+            return em.createQuery("SELECT s FROM Sportsman s", Sportsman.class).getResultList();
+        } catch (Throwable tr) {
+            throw new DataAccessException("Problem in find all sportsmans", tr);
+        }
     }
 
     @Override
     public List<Sportsman> findBySurname(String surname) {
-        TypedQuery<Sportsman> query =  em.createQuery("SELECT s FROM Sportsman s WHERE s.name = :name", Sportsman.class).setParameter("name",surname);
-        return query.getResultList();
+        try {
+            return em.createQuery("SELECT s FROM Sportsman s WHERE s.name = :name", Sportsman.class)
+                    .setParameter("name", surname).getResultList();
+        } catch (Throwable tr) {
+            throw new DataAccessException("Problem with find sportsman by surname: " + surname, tr);
+        }
     }
 
     @Override
     public Sportsman findByPersonalID(String id) {
-        return em.createQuery("select s from Sportsman s where s.personID = :id",Sportsman.class).setParameter("id",id).getSingleResult();
+        try {
+            return em.createQuery("select s from Sportsman s where s.personID = :id", Sportsman.class)
+                    .setParameter("id", id).getSingleResult();
+        } catch (NoResultException ex) {
+            return null;
+        } catch (Throwable tr) {
+            throw new DataAccessException("Problem with find sportsman by personal id: " + id, tr);
+        }
     }
 
     @Override
